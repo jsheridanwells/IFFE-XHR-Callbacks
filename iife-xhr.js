6a1f066adaec84fe19ae4e9dba8a1735f39c8fad
Jeremy Sheridan Wells <jsheridanwells@gmail.com>
2017-07-30 10:27:57 -0500

    improved IIFEs

--- iife-xhr.js
+++ iife-xhr.js
@@ -1,10 +1,10 @@
 function showCarnivores (carnivores) {
-  document.getElementById('carnivore-output').innerHTML = carnivores;
+  document.getElementById('carnivore-output').innerText = carnivores;
 }
 
 function showHerbivores (herbivores) {
-	document.getElementById('herbivore-output').innerHTML = herbivores;
+	document.getElementById('herbivore-output').innerText = herbivores;
 }
 
 Predator.loadCarnivores(showCarnivores);
-Prey.loadHerbivores(showHerbivores);
+Predator.loadHerbivores(showHerbivores);
